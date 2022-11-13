05c41e7761b50c589cfec473110ddad74265e4c6
Julio Dechert <julitod55@hotmail.com>
2022-11-12 23:45:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Practical work of algorithms
+### This work is done with a classmate
+
+Objective: learn about algorithms and how to implement them.
+The prompt is to choose 3 of the following algorithms and implement them to solve a problem
+- Breadth-first Search algorithm
+- Depth-first Search algorithm
+- Prim algorithm
+- Kruskal algorithm
+- Dijkstra algorithm
+- Floyd algorithm
+
+Chosen algorithms:
+- Prim algorithm
+- Kruskal algorithm
+- Dijkstra algorithm
+
+We choose cities of our country to use them like a graph to implement the algorithms.
---
+++ src/resources/GrafoTDA.java
@@ -0,0 +1,32 @@
+package resources;
+import apis.*;
+
+public interface GrafoTDA {
+
+    // Inicializa el grafo
+    void inicializarGrafo();
+
+    // Agrega un vertice
+    void agregarVertice(int var1);
+
+    // Borra un vertice
+    void eliminarVertice(int var1);
+
+    // Devuelve un conjunto de los vertices del grafo
+    ConjuntoTDA vertices();
+
+    // Agregar una arista
+    void agregarArista(int var1, int var2, int var3);
+
+    // Elimina una arista
+    void eliminarArista(int var1, int var2);
+
+    // Verifica si existe arista entre dos vertices
+    boolean existeArista(int var1, int var2);
+
+    // Devuelve el peso de la arista entre dos vertices
+    int pesoArista(int var1, int var2);
+
+    // Devuelve un conjunto de adyacentes de un vertice
+    ConjuntoTDA adyacentes(int v);
+}
